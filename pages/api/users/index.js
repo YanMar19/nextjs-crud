758afbb01a994c6ab213c1b81c03e8bbc07cd314
YanMar <ym2000gr@yahoo.com>
2022-06-16 20:43:24 +0300

    Update index.js

--- pages/api/users/index.js
+++ pages/api/users/index.js
@@ -2,13 +2,34 @@
 import { sql_query } from '../../../helpers/db';
 
 const handler = async (_, res) => {
-    try{
-        const results = await sql_query(`
-        SELECT * FROM Users
-        `);
-        return res.json(results);
-    } catch (e){
-        res.status(e.status).json({ status: e.status, message: e.message})
+    switch (req.method) {
+        case 'GET':
+            try{
+                const results = await sql_query(`
+                SELECT * FROM Users
+                `);
+                return res.json(results);
+            } catch (e){
+                res.status(e.status).json({ status: e.status, message: e.message})
+            }
+        case 'POST':
+            try{
+                const body = JSON.parse(req.body)
+                const results = await sql_query(`
+                SELECT * 
+                WHERE Users.email == ${body.email}
+                FROM Users
+                `);
+                if (results) {
+                    return res.status(e.status).json({ status: e.status, message: 'User with this email already exists'})
+                }
+
+                
+            } catch (e){
+                res.status(e.status).json({ status: e.status, message: e.message})
+            }
+        default:
+            return res.status(405).end(`Method ${req.method} Not Allowed`)
     }
 };
 
